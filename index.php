591a65de6a44216a209720cf6f1b8713a871f8cd
BinaryTENSHi <binarytenshi@gmail.com>
2014-10-01 14:48:45 +0200

    Title change

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $files = array_diff(scandir($PATH), array(".", ".."));
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Foundation | Welcome</title>
+    <title>Shellshock Live Log</title>
     <link rel="stylesheet" href="css/foundation.css"/>
     <script src="js/vendor/modernizr.js"></script>
 </head>
